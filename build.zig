59b9964a7efe72f44c67439dc4d4ca071a933b0e
leha <losos1337t@gmail.com>
2025-10-12 23:34:30 +0300

    Initial

---
+++ build.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "gaming",
+        .root_module = mod,
+    });
+    
+    b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+    
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_exe.step);
+}
