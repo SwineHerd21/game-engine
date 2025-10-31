fc2f86a4e572e8620403e86265995ce8d8f0f8f7
leha <losos1337t@gmail.com>
2025-10-31 18:33:46 +0300

    refactored native code selection

--- src/Window.zig
+++ src/Window.zig
@@ -3,16 +3,16 @@ const std = @import("std");
 
 const lib = @import("lib.zig");
 const events = @import("events.zig");
-const x11 = @import("native/x11.zig");
+const native = switch (lib.platform) {
+    .X11 => @import("native/x11.zig"),
+    .Windows => unreachable,
+};
 
 const Window = @This();
 
 
 /// Platform-specific implementation
-inner: switch (lib.platform) {
-    .X11 => x11.Context,
-    .Windows => unreachable,
-},
+inner: native.Context,
 width: u32,
 height: u32,
 /// Set to true when you want to close the window.
@@ -21,28 +21,19 @@ should_close: bool = false,
 /// Call Window.destroy when the window is no longer needed.
 pub fn createWindow(width: u32, height: u32, title: []const u8) !Window {
     // TODO: error handling
-    switch (lib.platform) {
-        .X11 => return .{
-            .inner = x11.createWindow(width, height, title),
-            .width = width,
-            .height = height,
-        },
-        .Windows => unreachable,
-    }
+    return .{
+        .inner = native.createWindow(width, height, title),
+        .width = width,
+        .height = height,
+    };
 }
 
 pub fn destroy(w: Window) void {
-    switch (lib.platform) {
-        .X11 => x11.closeWindow(w.inner),
-        .Windows => unreachable,
-    }
+    native.closeWindow(w.inner);
 }
 
 /// Consume the next OS event
 pub fn consumeEvent(w: *Window) ?events.Event {
-    switch (lib.platform) {
-        .X11 => return x11.consumeEvent(w),
-        .Windows => unreachable,
-    }
+    return native.consumeEvent(w);
 }
 
